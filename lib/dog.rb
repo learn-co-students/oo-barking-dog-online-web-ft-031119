fc23ecbad790ec1da2b255e6ae8dc07c8de3e400
TaylorSwinton <taylorswinton1@gmail.com>
2020-01-07 19:56:40 -0600

    Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Dog
+    def initialize(name)
+        @name = name
+    end
+
+    def name #getter method
+        @name
+    end
+
+    def name=(name) #setter method
+        @name = name
+    end
+
+    def bark
+        puts "woof!"
+    end
+end
+
